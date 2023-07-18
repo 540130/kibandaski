9734d9ec3d7048be03640cffb48e71807b9ca47e
brian <brian.kimuya@student.moringaschool.com>
2023-07-18 16:00:30 +0300

    done

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ foodOrder.addEventListener('click', () =>{
     const orderList = document.createElement('form')
     orderList.innerHTML =`
     <form>
-        <input type="text" id="food" placeholder="What food do you want"
+        <input type="text" id="food" placeholder="What food do you want?"
+        <input type="text" id="food" placeholder="To be delivered?"
     </form>`
     orderPlaced.innerHTML =orderList.innerHTML
     
